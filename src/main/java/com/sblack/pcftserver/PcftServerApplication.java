a44235b56cc15c1a89efc69630d8b0c1a5b0a7f2
Sean Black <black.sean14@gmail.com>
2022-04-11 14:11:24 -0700

    Init repo

---
+++ src/main/java/com/sblack/pcftserver/PcftServerApplication.java
@@ -0,0 +1,13 @@
+package com.sblack.pcftserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcftServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcftServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sblack/pcftserver/PcftServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sblack.pcftserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcftServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
