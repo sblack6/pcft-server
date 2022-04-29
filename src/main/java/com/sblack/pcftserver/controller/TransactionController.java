5298036f952f909e1507e92989940f963951907f
Sean Black <black.sean14@gmail.com>
2022-04-29 12:15:57 -0700

    Temporary search impl

--- src/main/java/com/sblack/pcftserver/controller/TransactionController.java
+++ src/main/java/com/sblack/pcftserver/controller/TransactionController.java
@@ -16,7 +16,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -60,41 +62,70 @@ public class TransactionController {
         return ResponseEntity.status(HttpStatus.OK).body(SUCCESS);
     }
 
+    // TODO: Temporary search method impl to support frontend POC.
+    //  Refactor search leveraging Spring JPA Criteria and/or specifications.
     @GetMapping("/search")
-    public ResponseEntity search(@RequestParam(required = false) String startDate, @RequestParam(required = false) String endDate, @RequestParam(required = false) String type, @RequestParam(required = false) String category, @RequestParam(required = false) String tags) {
-        log.info("Search request got params: {}, {}", startDate, endDate);
-        DateSearchConstraints dateSearch = this.validateSearchByDateParams(startDate, endDate);
-        log.info("Date search constraints: {}", dateSearch);
-        List<Transaction> dateSearchResults = null;
-        List<Transaction> exampleSearchResults = null;
-        if (dateSearch.isDateSearch()) {
-            if (dateSearch.isValid()) {
-                log.info("Searching by date...");
-                dateSearchResults = this.transactionsRepo.findAllByDateBetween(dateSearch.getStart(), dateSearch.getEnd());
+    public ResponseEntity search(@RequestParam(required = false) String startDate, @RequestParam(required = false) String endDate, @RequestParam(required = false) String type, @RequestParam(required = false) String category, @RequestParam(required = false) String tag) {
+        List<Transaction> dateSearchResults;
+        try {
+            DateSearchConstraints dateSearch = this.validateSearchByDateParams(startDate, endDate);
+            dateSearchResults = dateSearch(dateSearch);
+            log.info("Date search results: {}", dateSearchResults);
+        } catch (PcftException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
+        }
+
+        List<Transaction> exampleSearchResults = exampleSearch(type, category);
+        log.info("Example search results: {}", exampleSearchResults);
+
+        List<Transaction> combinedResults;
+        if (!exampleSearchResults.isEmpty()) {
+            if (!dateSearchResults.isEmpty()) {
+                exampleSearchResults.retainAll(dateSearchResults);
+            }
+            combinedResults = exampleSearchResults;
+        } else {
+            combinedResults = dateSearchResults;
+        }
+        List<Transaction> finalResults = tagSearch(combinedResults, tag);
+        return ResponseEntity.status(HttpStatus.OK).body(finalResults);
+    }
+
+    // TODO: Temporary method to support frontend POC
+    private List<Transaction> tagSearch(List<Transaction> transactionList, String tag) {
+        if (tag != null && transactionList != null) {
+            transactionList = transactionList.stream()
+                    .filter(transaction -> transaction.getTags().toLowerCase().contains(tag.toLowerCase()))
+                    .collect(Collectors.toList());
+        }
+        return transactionList;
+    }
+
+    // TODO: temporary method to support frontend POC.
+    private List<Transaction> dateSearch(DateSearchConstraints searchConstraints) throws PcftException {
+        List<Transaction> searchResults = new LinkedList<>();
+        if (searchConstraints.isDateSearch()) {
+            if (searchConstraints.isValid()) {
+                searchResults = this.transactionsRepo.findAllByDateBetween(searchConstraints.getStart(), searchConstraints.getEnd());
             } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dateSearch.getErrorMessage());
+                throw new PcftException(searchConstraints.getErrorMessage());
             }
         }
-        if (type != null || category != null || tags != null) {
+        return searchResults;
+    }
+
+    // TODO: temporary method to support frontend POC.
+    private List<Transaction> exampleSearch(String type, String category) {
+        if (type != null || category != null) {
             Transaction.TransactionBuilder exampleBuilder = Transaction.builder()
-                    .category(category)
-                    .tags(tags);
+                    .category(category);
             if (type != null) {
                 TransactionType transactionType = TransactionType.valueOf(type.toUpperCase());
                 exampleBuilder.type(transactionType);
             }
-            exampleSearchResults = transactionsRepo.findAll(Example.of(exampleBuilder.build()));
+            return transactionsRepo.findAll(Example.of(exampleBuilder.build()));
         }
-        if (exampleSearchResults != null) {
-            if (dateSearchResults != null) {
-                exampleSearchResults.retainAll(dateSearchResults);
-                return ResponseEntity.status(HttpStatus.OK).body(exampleSearchResults);
-            } else {
-                return ResponseEntity.status(HttpStatus.OK).body(exampleSearchResults);
-            }
-        } else {
-            return ResponseEntity.status(HttpStatus.OK).body(dateSearchResults);
-        }
+        return new LinkedList<>();
     }
 
     @PostMapping("/upload-transactions")
